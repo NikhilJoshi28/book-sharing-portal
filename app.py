34d3ad32bba4ac8a474f1744e36369eca275119b
enigmaeth <enigmaeth@gmail.com>
2017-04-11 18:57:44 +0530

    added requirements.txt, signup function

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,flash,redirect,url_for,session
+from flask_wtf import Form
+from wtforms import BooleanField,StringField,PasswordField,validators
+from passlib.hash import sha256_crypt
+from MySQLdb import escape_string as thwart
+import gc
+
 from scripts import dbconnect
 from content_management import Content
-from flask_wtf import Form
-from wtforms import BooleanField,validators,StringField,PasswordField
 
 
 class RegistrationForm(Form):
@@ -40,11 +44,41 @@ def dashboard():
 @app.route('/signup/', methods=["GET","POST"])
 def signup():
     try:
-        c, conn = dbconnect.connection()
-        print("okay")
-        return("okay")
+        form = RegistrationForm(request.form)
+
+        if request.method == "POST" and form.validate():
+            username = form.username.data
+            email = form.email.data
+            password = sha256_crypt.encrypt((str(form.password.data)))
+            c, conn = dbconnect.connection()
+
+            x = c.execute("SELECT * FROM users WHERE username = (%s)",
+                          (thwart(username)))
+
+            if int(x) > 0:
+                flash("That username is already taken, please choose another")
+                return render_template('register.html', form=form)
+
+            else:
+                c.execute("INSERT INTO users (username, password, email, tracking) VALUES (%s, %s, %s, %s)",
+                          (thwart(username), thwart(password), thwart(email),
+                           thwart("/introduction-to-python-programming/")))
+
+                conn.commit()
+                flash("Thanks for registering!")
+                c.close()
+                conn.close()
+                gc.collect()
+
+                session['logged_in'] = True
+                session['username'] = username
+
+                return redirect(url_for('dashboard'))
+
+        return render_template("register.html", form=form)
+
     except Exception as e:
-        return(str(e))
+        return (str(e))
 
 @app.errorhandler(404)
 def pageNotFound(e):
