8e5dd74cd2bfb6c011ba72e655c0688848d66b7c
enigmaeth <enigmaeth@gmail.com>
2017-04-11 16:15:18 +0530

    Added Flask-WTF, database

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask,render_template
 from scripts import dbconnect
-from flask import Flask,render_template, flash
 from content_management import Content
+from flask_wtf import Form
+from wtforms import BooleanField,validators,StringField,PasswordField
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=20)])
+    email = StringField('Email Address', [validators.Length(min=6, max=50)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)',
+                              [validators.Required()])
 
 BOOK_DETAILS= Content()
 
@@ -9,7 +22,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    dbconnect.hello();
     return render_template("main.html")
 
 @app.route('/login/')
@@ -25,15 +37,18 @@ def dashboard():
     #else:
         #promt error on login form
 
-@app.route('/signup')
+@app.route('/signup/', methods=["GET","POST"])
 def signup():
-    print("Signing up")
-    #signup function
+    try:
+        c, conn = dbconnect.connection()
+        print("okay")
+        return("okay")
+    except Exception as e:
+        return(str(e))
 
 @app.errorhandler(404)
 def pageNotFound(e):
     return render_template("404.html")
-
 
 if __name__=='__main__':
       app.run(host='0.0.0.0', port=4141, debug=True, threaded=True)
