ddeb63d846f59f6a3b3e9c0996cce49eab8c1d1b
Nikhiljoshi28 <nikhiljoshi1nikhil@gmail.com>
2017-04-11 15:25:41 +0530

    content managegement Modified

--- content_management.py
+++ content_management.py
@@ -1,7 +1,7 @@
 
 def Content():
     BOOK_DETAILS= {"Book":[["Introduction To Algorithms","Second","Thomas Cormen","Available","/link0/"],
-                            ["Artificial Intelligence a","Third","Stuart J.Russell","Available","/link1/"],
+                            ["Artificial Intelligence A Modern Approch","Third","Stuart J.Russell","Available","/link1/"],
                             ["Fundamentals of Database System","Fifth","Ramez Elmasri","Not Available","/link2/"]]}
 
     return BOOK_DETAILS
