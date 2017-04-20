794fa13144d0f09c7f25664abf3046fe6b800814
enigmaeth <enigmaeth@gmail.com>
2017-04-20 13:46:39 +0530

    working stable

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from passlib.hash import sha256_crypt
 from wtforms import StringField, validators, PasswordField, Form
 
 from dbconnect import connection
-from content_management import Content, searchContent, searchByID, requestsByBorrower, requestsByLender, getDate
+from content_management import Content, searchContent, searchByID, requestsByBorrower, requestsByLender, getDate, approveR, getUserSQL
 
 app = Flask(__name__)
 app.secret_key = 'beae135cce92ebf432b043c193ccf78eb8a986f8f9febcb7f14f64ba6bc9c387'
@@ -138,14 +138,14 @@ def dashboard():
                 conn.close()
                 gc.collect()
             else:
-                print("duplicate entry")
+                print("duplicate entry for this book")
                 #add flash message or something here
 
-        return render_template("dashboard.html", Book_details=BOOK_DETAILS)
+        return render_template("dashboard.html", username = session['userID'] ,Book_details=BOOK_DETAILS)
 
     except Exception as e:
         print e
-    return render_template("dashboard.html", Book_details=BOOK_DETAILS)
+    return render_template("dashboard.html", username = session['userID'] ,Book_details=BOOK_DETAILS)
 
 @app.route('/confirmation/')
 def confirmation():
@@ -160,13 +160,13 @@ def search():
     if request.method == "POST":
         query = request.form['search']
         BOOK_DETAILS=searchContent(query)
-    return render_template("dashboard.html", Book_details=BOOK_DETAILS)
+    return render_template("dashboard.html", username = session['userID'] ,Book_details=BOOK_DETAILS)
 
 @app.route('/booksShared/', methods=['POST'])
 def booksShared():
     if request.method == "POST":
         BOOKS_SHARED=searchByID(session['userID'])
-    return render_template("dashboard.html", Books_shared=BOOKS_SHARED)
+    return render_template("dashboard.html", username = session['userID'] ,Books_shared=BOOKS_SHARED)
 
 @app.route('/changePassword/', methods = ['POST'] )
 def changePassword():
@@ -207,7 +207,7 @@ def changePassword():
     except Exception as e:
             print e
             print "AAAAAAa"
-    return render_template("dashboard.html", Book_details=BOOK_DETAILS)
+    return render_template("dashboard.html", username = session['userID'] ,Book_details=BOOK_DETAILS)
 
 @app.route('/sendRequest/', methods=['POST'])
 def sendRequest():
@@ -221,33 +221,65 @@ def sendRequest():
         borrowerID = str(session['userID'])
         requestID = lenderID+bookID+borrowerID
         approvalStatus = 0
-
+        print 65465465
+        print (startDate), (endDate), (int)(approvalStatus), (requestID), (lenderID), (borrowerID), (bookID)
         c, conn = connection()
         x = c.execute("SELECT * FROM requests WHERE requestID = %s", (requestID,))
         if int(x < 1):
-            c.execute("INSERT INTO requests VALUES ( %s, %s, %s, %s, %s, %s)",
-                      ((startDate), (endDate), (int)(approvalStatus), (requestID), (lenderID), (borrowerID)))
+            c.execute("INSERT INTO requests VALUES ( %s, %s, %s, %s, %s, %s, %s)",
+                      ((startDate), (endDate), (int)(approvalStatus), (requestID), (lenderID), (borrowerID), (bookID)))
             print("inserted")
             conn.commit()
             c.close()
             conn.close()
             gc.collect()
         else:
-            print("duplicate entry")
-            # add flash message or something here
-    return render_template("dashboard.html", Book_details=BOOK_DETAILS)
+            print("duplicate entry for this request")
+    return render_template("dashboard.html", username = session['userID'] ,Book_details=BOOK_DETAILS)
 
 @app.route('/incomingRequests/', methods=['POST'])
 def incomingRequests():
     if request.method == "POST":
         INCOMING_REQUESTS = requestsByLender(session['userID'])
-        return render_template("dashboard.html", Requests=INCOMING_REQUESTS)
+        return render_template("dashboard.html", username = session['userID'] , iRequests=INCOMING_REQUESTS)
 
 @app.route('/sentRequests/', methods=['POST'])
 def sentRequests():
     if request.method == "POST":
         SENT_REQUESTS = requestsByBorrower(session['userID'])
-        return render_template("dashboard.html", Requests=SENT_REQUESTS)
+        return render_template("dashboard.html", username = session['userID'] , Requests=SENT_REQUESTS)
+
+@app.route('/approve/',methods=['POST'])
+def approve():
+    if request.method == "POST":
+        requestID = request.form['loopID']
+        print(requestID)
+        approveR(requestID)
+        return render_template("dashboard.html", username=session['userID'], Requests=INCOMING_REQUESTS)
+
+@app.route('/getLender', methods=['POST'])
+def getLender():
+    if request.method == "POST":
+        userID = request.form['lenderID']
+        DETAILS = getUserSQL(userID)
+        return render_template("user.html", username=session['userID'], Details=DETAILS)
+
+@app.route('/getBorrower', methods=['POST'])
+def getBorrower():
+    if request.method == "POST":
+        userID = request.form['borrowerID']
+        DETAILS = getUserSQL(userID)
+        return render_template("user.html", username=session['userID'], Details=DETAILS)
+
+
+@app.route('/logout/', methods=['POST'])
+def logout():
+    session.clear()
+
+@app.after_request
+def set_header(r):
+    r.headers["Cache-Control"] = "no-cache"
+    return r
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=4142, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=4141, debug=True, threaded=True)
--- content_management.py
+++ content_management.py
@@ -3,6 +3,7 @@ from dbconnect import connection
 c, conn = connection()
 BOOK_DETAILS = []
 REQUESTS = []
+DETAILS = []
 
 def execute_query(sqlQ):
     BOOK_DETAILS = []
@@ -21,12 +22,22 @@ def execute_id(sqlQ):
     c.execute(sqlQ)
     data = c.fetchall()
     for row in data:
-        request = [row[0], row[1], row[2], row[3], row[4], row[5]]
+        request = [row[0], row[1], row[2], row[3], row[4], row[5], row[6]]
         REQUESTS.append(request)
     conn.commit()
     gc.collect()
 
     return REQUESTS
+
+def executeUser_id(sqlQ):
+    DETAILS = []
+    c.execute(sqlQ)
+    data = c.fetchall()
+    for row in data:
+        detail = [row[0], row[1], row[2], row[3], row[4]]
+        DETAILS.append(detail)
+    conn.commit()
+    gc.collect()
 
 
 def Content():
@@ -48,6 +59,17 @@ def requestsByLender(id):
 def requestsByBorrower(id):
     sqlQ = "select * from requests where borrowerID = '" + str(id) + "' order by startDate asc"
     return execute_id(sqlQ)
+
+def approveR(id):
+    sqlQ = "update requests set approvalStatus = 1 where requestID = '" + str(id) + "'"
+    c.execute(sqlQ)
+    print "executed"
+    conn.commit()
+    gc.collect()
+
+def getUserSQL(id):
+    sqlQ = ""
+    return executeUser_id(sqlQ)
 
 def getDate():
     sqlQ = "Date()"
